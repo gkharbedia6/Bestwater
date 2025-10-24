9ab210b941951334d1adc250e41e0a1269f96605
Giorgi Kharbedia <giorgikharbedia6@gmail.com>
2025-10-24 13:30:07 +0400

    under construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { Menu } from "./components/Menu";
-
 function App() {
   return (
     <>
-      <div className="flex w-full justify-center align-center h-24">
-        <Menu />
+      <div className="flex w-full justify-center align-center h-screen">
+        <div className="flex flex-col w-full items-center justify-center gap-2">
+          <h1 className="text-3xl bold animate animate-pulse">
+            Website is under construction <span>...</span>
+          </h1>
+          <h2 className="text-xl italic">Will be back in 7 days</h2>
+        </div>
       </div>
     </>
   );
